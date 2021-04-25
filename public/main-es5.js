711926e5f17c838255c34cd58ce56fe8a25d860e
karnkumar123 <karnkumar.git@gmail.com>
2021-04-25 19:26:52 +0530

    add files for getting started with material

--- public/main-es5.js
+++ public/main-es5.js
@@ -30,7 +30,29 @@ webpackEmptyAsyncContext.id = "./$$_lazy_route_resource lazy recursive";
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<!--The content below is only a placeholder and can be replaced.-->\n<div>\n  <h1 style=\"background-color: brown;\">\n    We will add more cool videos here soon !! <br>\n    keep in touch\n  </h1>\n</div>\n\n<router-outlet></router-outlet>\n"
+module.exports = "<!--The content below is only a placeholder and can be replaced.-->\n<app-getting-started></app-getting-started>\n\n<router-outlet></router-outlet>\n"
+
+/***/ }),
+
+/***/ "./node_modules/raw-loader/index.js!./src/app/components/getting-started/getting-started-content/getting-started-content.component.html":
+/*!*************************************************************************************************************************************!*\
+  !*** ./node_modules/raw-loader!./src/app/components/getting-started/getting-started-content/getting-started-content.component.html ***!
+  \*************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "<div>\n    It is very simple to add material in the angular-application.\n</div>\n<div>\n    Here are the few steps to getting started with material design.\n</div>\n<div>\n    <ul>\n        <li>\n            Go to angular application root folder.\n        </li>\n        <li>\n            Give command <b>ng add @angular/material</b>\n        </li>\n    </ul>\n</div>\n\n<div>\n    This command will install all the supported packages automatically.\n</div>\n\n<div>\n    <ul>\n        <li>\n            Go to any module.ts file (prefer app.module.ts file), the root module.\n        </li>\n        <li>\n            We will import material component say button, expansion, dropdown etc here in app.module.ts file.\n        </li>\n        <li>\n            Lets say we have to use material button in our application.\n            <ul>\n                <li>\n                    <b>{{importModuleStr}}</b> in app.module.ts file.\n                </li>\n                <li>\n                    Add <b>MatButtonModule</b> in the imports array in app.module.ts file.\n                </li>\n                <li>\n                    Move to <b>app.component.html file (any html file)</b>.\n                </li>\n                <li>\n                    Add button as : {{buttonStr}}\n                </li>\n                <li>\n                    Run <b>ng serve</b> command and see the results.\n                </li>\n                <li> \n                    If you are seeing more beautiful button then You will ready to start to play with <b><i>material.</i></b>\n                 </li>\n            </ul>\n        </li>\n    </ul>\n</div>"
+
+/***/ }),
+
+/***/ "./node_modules/raw-loader/index.js!./src/app/components/getting-started/getting-started.component.html":
+/*!*****************************************************************************************************!*\
+  !*** ./node_modules/raw-loader!./src/app/components/getting-started/getting-started.component.html ***!
+  \*****************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "<mat-expansion-panel>\n    <mat-expansion-panel-header>\n        <mat-panel-title>\n            Getting Started\n        </mat-panel-title>\n        <mat-panel-description>\n            Total Duration: 15 mins\n        </mat-panel-description>\n    </mat-expansion-panel-header>\n    <app-getting-started-content></app-getting-started-content>\n</mat-expansion-panel>\n"
 
 /***/ }),
 
@@ -125,6 +147,14 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
 /* harmony import */ var _app_routing_module__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./app-routing.module */ "./src/app/app-routing.module.ts");
 /* harmony import */ var _app_component__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./app.component */ "./src/app/app.component.ts");
+/* harmony import */ var _material_material_module__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./material/material.module */ "./src/app/material/material.module.ts");
+/* harmony import */ var _angular_platform_browser_animations__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! @angular/platform-browser/animations */ "./node_modules/@angular/platform-browser/fesm5/animations.js");
+/* harmony import */ var _components_getting_started_getting_started_component__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ./components/getting-started/getting-started.component */ "./src/app/components/getting-started/getting-started.component.ts");
+/* harmony import */ var _components_getting_started_getting_started_content_getting_started_content_component__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! ./components/getting-started/getting-started-content/getting-started-content.component */ "./src/app/components/getting-started/getting-started-content/getting-started-content.component.ts");
+
+
+
+
 
 
 
@@ -136,17 +166,152 @@ var AppModule = /** @class */ (function () {
     AppModule = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
         Object(_angular_core__WEBPACK_IMPORTED_MODULE_2__["NgModule"])({
             declarations: [
-                _app_component__WEBPACK_IMPORTED_MODULE_4__["AppComponent"]
+                _app_component__WEBPACK_IMPORTED_MODULE_4__["AppComponent"],
+                _components_getting_started_getting_started_component__WEBPACK_IMPORTED_MODULE_7__["GettingStartedComponent"],
+                _components_getting_started_getting_started_content_getting_started_content_component__WEBPACK_IMPORTED_MODULE_8__["GettingStartedContentComponent"]
             ],
             imports: [
                 _angular_platform_browser__WEBPACK_IMPORTED_MODULE_1__["BrowserModule"],
-                _app_routing_module__WEBPACK_IMPORTED_MODULE_3__["AppRoutingModule"]
+                _app_routing_module__WEBPACK_IMPORTED_MODULE_3__["AppRoutingModule"],
+                _material_material_module__WEBPACK_IMPORTED_MODULE_5__["MaterialModule"],
+                _angular_platform_browser_animations__WEBPACK_IMPORTED_MODULE_6__["BrowserAnimationsModule"]
             ],
             providers: [],
             bootstrap: [_app_component__WEBPACK_IMPORTED_MODULE_4__["AppComponent"]]
         })
     ], AppModule);
     return AppModule;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/components/getting-started/getting-started-content/getting-started-content.component.scss":
+/*!***********************************************************************************************************!*\
+  !*** ./src/app/components/getting-started/getting-started-content/getting-started-content.component.scss ***!
+  \***********************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJzcmMvYXBwL2NvbXBvbmVudHMvZ2V0dGluZy1zdGFydGVkL2dldHRpbmctc3RhcnRlZC1jb250ZW50L2dldHRpbmctc3RhcnRlZC1jb250ZW50LmNvbXBvbmVudC5zY3NzIn0= */"
+
+/***/ }),
+
+/***/ "./src/app/components/getting-started/getting-started-content/getting-started-content.component.ts":
+/*!*********************************************************************************************************!*\
+  !*** ./src/app/components/getting-started/getting-started-content/getting-started-content.component.ts ***!
+  \*********************************************************************************************************/
+/*! exports provided: GettingStartedContentComponent */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "GettingStartedContentComponent", function() { return GettingStartedContentComponent; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+
+
+var GettingStartedContentComponent = /** @class */ (function () {
+    function GettingStartedContentComponent() {
+        this.importModuleStr = "import {MatButtonModule} from '@angular/material/button';";
+        this.buttonStr = "<button mat-button>My Awesome Button</button>";
+    }
+    GettingStartedContentComponent.prototype.ngOnInit = function () {
+    };
+    GettingStartedContentComponent = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Component"])({
+            selector: 'app-getting-started-content',
+            template: __webpack_require__(/*! raw-loader!./getting-started-content.component.html */ "./node_modules/raw-loader/index.js!./src/app/components/getting-started/getting-started-content/getting-started-content.component.html"),
+            styles: [__webpack_require__(/*! ./getting-started-content.component.scss */ "./src/app/components/getting-started/getting-started-content/getting-started-content.component.scss")]
+        })
+    ], GettingStartedContentComponent);
+    return GettingStartedContentComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/components/getting-started/getting-started.component.scss":
+/*!***************************************************************************!*\
+  !*** ./src/app/components/getting-started/getting-started.component.scss ***!
+  \***************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJzcmMvYXBwL2NvbXBvbmVudHMvZ2V0dGluZy1zdGFydGVkL2dldHRpbmctc3RhcnRlZC5jb21wb25lbnQuc2NzcyJ9 */"
+
+/***/ }),
+
+/***/ "./src/app/components/getting-started/getting-started.component.ts":
+/*!*************************************************************************!*\
+  !*** ./src/app/components/getting-started/getting-started.component.ts ***!
+  \*************************************************************************/
+/*! exports provided: GettingStartedComponent */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "GettingStartedComponent", function() { return GettingStartedComponent; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+
+
+var GettingStartedComponent = /** @class */ (function () {
+    function GettingStartedComponent() {
+    }
+    GettingStartedComponent.prototype.ngOnInit = function () {
+    };
+    GettingStartedComponent = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Component"])({
+            selector: 'app-getting-started',
+            template: __webpack_require__(/*! raw-loader!./getting-started.component.html */ "./node_modules/raw-loader/index.js!./src/app/components/getting-started/getting-started.component.html"),
+            styles: [__webpack_require__(/*! ./getting-started.component.scss */ "./src/app/components/getting-started/getting-started.component.scss")]
+        })
+    ], GettingStartedComponent);
+    return GettingStartedComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/material/material.module.ts":
+/*!*********************************************!*\
+  !*** ./src/app/material/material.module.ts ***!
+  \*********************************************/
+/*! exports provided: MaterialModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "MaterialModule", function() { return MaterialModule; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _angular_common__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/common */ "./node_modules/@angular/common/fesm5/common.js");
+/* harmony import */ var _angular_material__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @angular/material */ "./node_modules/@angular/material/esm5/material.es5.js");
+
+
+
+
+var MaterialComponents = [
+    _angular_material__WEBPACK_IMPORTED_MODULE_3__["MatExpansionModule"]
+];
+var MaterialModule = /** @class */ (function () {
+    function MaterialModule() {
+    }
+    MaterialModule = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
+            declarations: [],
+            imports: [
+                _angular_common__WEBPACK_IMPORTED_MODULE_2__["CommonModule"],
+                MaterialComponents
+            ],
+            exports: [MaterialComponents]
+        })
+    ], MaterialModule);
+    return MaterialModule;
 }());
 
 
@@ -190,18 +355,21 @@ var environment = {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
-/* harmony import */ var _angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/platform-browser-dynamic */ "./node_modules/@angular/platform-browser-dynamic/fesm5/platform-browser-dynamic.js");
-/* harmony import */ var _app_app_module__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./app/app.module */ "./src/app/app.module.ts");
-/* harmony import */ var _environments_environment__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./environments/environment */ "./src/environments/environment.ts");
+/* harmony import */ var hammerjs__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! hammerjs */ "./node_modules/hammerjs/hammer.js");
+/* harmony import */ var hammerjs__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(hammerjs__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/platform-browser-dynamic */ "./node_modules/@angular/platform-browser-dynamic/fesm5/platform-browser-dynamic.js");
+/* harmony import */ var _app_app_module__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./app/app.module */ "./src/app/app.module.ts");
+/* harmony import */ var _environments_environment__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./environments/environment */ "./src/environments/environment.ts");
 
 
 
 
-if (_environments_environment__WEBPACK_IMPORTED_MODULE_3__["environment"].production) {
-    Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["enableProdMode"])();
+
+if (_environments_environment__WEBPACK_IMPORTED_MODULE_4__["environment"].production) {
+    Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["enableProdMode"])();
 }
-Object(_angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__["platformBrowserDynamic"])().bootstrapModule(_app_app_module__WEBPACK_IMPORTED_MODULE_2__["AppModule"])
+Object(_angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_2__["platformBrowserDynamic"])().bootstrapModule(_app_app_module__WEBPACK_IMPORTED_MODULE_3__["AppModule"])
     .catch(function (err) { return console.error(err); });
 
 
