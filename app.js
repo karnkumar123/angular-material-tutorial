3fcb9b654a25265164bed557fae4c24d77ea101b
karnkumar123 <karnkumar.git@gmail.com>
2021-04-25 18:16:53 +0530

    adding initial files

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.listen(PORT, () => {
+    console.log(`Server is started at PORT ${PORT}`);
+})
